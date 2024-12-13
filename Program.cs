c040423651b4c930d8bc9b6ae5375bc2845b9133
Veslo5 <petrveslo55@gmail.com>
2024-12-13 18:19:14 +0100

    new if branch

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+var x = 5;
+
+if(x == 5)
+{
+	Console.WriteLine("x = 5!");	
+}else 
+{
+	Console.WriteLine("x is not 5");	
+}
